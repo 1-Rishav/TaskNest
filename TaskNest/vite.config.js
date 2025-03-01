002ec0c4754db86f8305f4bbb261924643e5604a
Rishav Raj <rajrishav011@gmail.com>
2025-03-01 22:51:48 +0530

    update vite config

--- TaskNest/vite.config.js
+++ TaskNest/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: '_dist' // Change 'output' to your desired folder name
+  },
   plugins: [react()],
 })
